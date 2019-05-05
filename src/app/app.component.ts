2ab4885a6e097c48b54e663f3d53ecb7df3c6193
Riadh BEL HADJ SALAH <riadh_bel_hadj2004@hotmail.com>
2019-05-05 14:17:28 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  public serverElements = [{type:'server',  name:'Test server', content:'Just a test'}];
+  
+
+}
